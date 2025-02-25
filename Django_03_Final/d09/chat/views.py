6405b921a9bb4943fca97ef1c110a8182c1fd17b
josephcheel <josephcheelnakashima@gmail.com>
2025-02-25 12:38:27 +0100

    final exercise finished

---
+++ Django_03_Final/d09/chat/views.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+from .models import chatRoom
+# Create your views here.
+
+@login_required(login_url='/')
+def chat_view(request, *args, **kwargs):
+	from django.shortcuts import render, redirect
+from django.http import Http404
+
+def chat_view(request, *args, **kwargs):
+	room_name = kwargs.get('room_name')
+
+	if room_name:
+		try:
+			chat_room = chatRoom.objects.get(name=room_name)
+		except chatRoom.DoesNotExist:
+			return redirect('chat:index')
+
+		last_three_messages = chat_room.last_three_messages.all()
+		print(type(last_three_messages))
+		for message in last_three_messages:
+			print(message.message)
+		return render(request, 'chat.html', {
+			'chatRoom': chat_room,
+			'messages': last_three_messages
+		})
+
+	else:
+		return render(request, 'chatrooms_list.html', {
+			'chatrooms': chatRoom.objects.all()
+		})
