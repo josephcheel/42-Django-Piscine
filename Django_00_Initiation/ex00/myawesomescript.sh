74e104a14c8d4729ece1ed06e4f56bdae0c2fd74
josephcheel <josephcheelnakashima@gmail.com>
2025-02-25 13:43:07 +0100

    corrected small error

--- Django_00_Initiation/ex00/myawesomescript.sh
+++ Django_00_Initiation/ex00/myawesomescript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -z $1 ]; then
-	curl -sSI $1 | grep location | cut -d ":" -f 2-
+	curl -sSI $1 | grep -i location | cut -d ":" -f 2-
 else
 	echo "usage: ./myawesomescript.sh [ex: https://www.42barcelona.com/]"
 fi
